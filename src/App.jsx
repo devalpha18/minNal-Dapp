0bd847163a94dcd472b75f9c597e676640fa88b2
smile930307 <koriumemra0307@gmail.com>
2024-02-07 02:21:13 -0800

    initial version

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { ethers } from "ethers";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import JoviToken from './assets/JoviToken.json'
-import JoviTokenCrowdsale from './assets/JoviTokenCrowdsale.json';
+import ICO_Abi from './assets/ICO.json'
+import Token_Abi from './assets/Token.json'
 
 // Constants
-const TOKEN_ADDRESS = '0x3CF0E053EE1b554E5054300B56EBa1EaD7162a25';
-const CROWDSALE_ADDRESS = '0xfD8c0325BA3520bCF9399d7EACBAe8cb00369765';
-const TWITTER_HANDLE = 'DsouzaJovian';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const ICO_ADDRESS = '0x76B4084209Eb15754983788bd8e3cb0E9c631b3A';
+const TOKEN_ADDRESS = "0x9Ab5c04Ef221ee48f344C114f2233fA4ee896fc2";
 
 function Footer() {
   return (
     <div className="footer-container">
       <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-      <a
-        className="footer-text"
-        href={TWITTER_LINK}
-        target="_blank"
-        rel="noreferrer"
-      > {`built by @${TWITTER_HANDLE}`} </a>
     </div>
   );
 }
 
 function App() {
+  const [balanceAmout, setBalanceAmout] = useState({ICO_balance:"", Account_balance:""})
+  useEffect(() => {
+    getBalance()
+    console.log("balanceAmout", balanceAmout)
+  }, [balanceAmout])
 
   const [currentAccount, setCurrentAccount] = useState("");
-
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
     if (!ethereum) {
@@ -73,121 +69,63 @@ function App() {
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const crowdsale = new ethers.Contract(CROWDSALE_ADDRESS, JoviTokenCrowdsale.abi, signer);
-
-        let nftTxn = await crowdsale.buyTokens(currentAccount, { value: ethers.utils.parseEther(amount) });
-
-        await nftTxn.wait();
-        console.log(`Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`);
-
+        const Token = new ethers.Contract(ICO_ADDRESS, ICO_Abi.abi, signer);
+        const tokenAmountInEther = (ethers.utils.parseEther((0.015 * Number(amount)).toString()));
+        const gasPrice = await provider.getGasPrice();
+        const options = {
+          gasPrice,
+          gasLimit: ethers.utils.parseUnits("200000", 0),
+          value: tokenAmountInEther
+        };
+        let tx = await Token.buy(options);
+        console.log("Buy", tx);
       } else {
         console.log("Ethereum object doesn't exist!");
       }
     }
+
     catch (error) {
       console.log(error)
     }
   }
 
-  async function getTotalSupply() {
+  const withdrawToken = async (amount) => {
     try {
       const { ethereum } = window;
-
+    
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const contract = new ethers.Contract(TOKEN_ADDRESS, JoviToken.abi, signer);
-
-        let totalSupply = await contract.totalSupply();
-        let decimals = await contract.decimals();
-        totalSupply = ethers.utils.formatUnits(totalSupply, decimals);
-        totalSupply = ethers.utils.commify(totalSupply);
-
-        return totalSupply;
-
+        const Token = new ethers.Contract(ICO_ADDRESS, ICO.abi, signer);
+        const tokenAmountInEther = ethers.utils.parseEther((0.015 * Number(amount)).toString());
+        console.log("tokenAmountInEther", tokenAmountInEther)
+        const gasPrice = await provider.getGasPrice();
+        const options = {
+          gasPrice,
+          gasLimit: 300000, // Set gas limit as a number
+          value: tokenAmountInEther
+        };
+        console.log("options", options)
+        let tx = await Token.withdraw(ethers.utils.parseEther(amount));
+        console.log("Buy", tx);
       } else {
         console.log("Ethereum object doesn't exist!");
       }
     }
+    
     catch (error) {
       console.log(error)
     }
   }
 
-  async function registerTotalSupplyChange(onChange) {
-    try {
-      const { ethereum } = window;
+  const getBalance = async () => {
+    const provider = new ethers.providers.JsonRpcProvider("https://rpc.mordor.etccooperative.org");
 
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const contract = new ethers.Contract(TOKEN_ADDRESS, JoviToken.abi, signer);
-        const decimals = await contract.decimals();
-
-        contract.on("Transfer", (from, to, value, event) => {
-          if (from === ethers.constants.AddressZero) {
-            // const supplyIncrease = ethers.utils.formatUnits(value, decimals);
-            onChange();
-          }
-        })
-
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }
-    }
-    catch (error) {
-      console.log(error)
-    }
+    const tokenContract = new ethers.Contract(TOKEN_ADDRESS, Token_Abi.abi, provider);
+    const ICO_balance = await tokenContract.balanceOf(ICO_ADDRESS)
+    const Account_balance = await tokenContract.balanceOf(ethereum.selectedAccount)
+      setBalanceAmout(ICO_balance.toString(), Account_balance.toString())
   }
-
-  async function getCrowdsaleData() {
-    try {
-      const { ethereum } = window;
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const crowdsale = new ethers.Contract(CROWDSALE_ADDRESS, JoviTokenCrowdsale.abi, signer);
-        const contract = new ethers.Contract(TOKEN_ADDRESS, JoviToken.abi, signer);
-
-        // closing time
-        let closingTime = await crowdsale.closingTime();
-        closingTime = closingTime.mul(1000).toNumber();
-
-        // stage
-        let stage = await crowdsale.stage();
-        if (stage == 0) {
-          stage = "Presale";
-        }
-        else {
-          stage = "ICO";
-        }
-
-        // rate
-        let rate = await crowdsale.rate();
-        rate = rate.toNumber();
-
-        // investor min max amount
-        let decimals = await contract.decimals();
-        let investorMinAmount = await crowdsale.investorMinCap();
-        investorMinAmount = ethers.utils.formatUnits(investorMinAmount, decimals);
-        investorMinAmount = investorMinAmount.toString();
-        let investorMaxAmount = await crowdsale.investorMaxCap();
-        investorMaxAmount = ethers.utils.formatUnits(investorMaxAmount, decimals);
-        investorMaxAmount = investorMaxAmount.toString();
-
-
-        return { closingTime, stage, rate, investorMinAmount, investorMaxAmount };
-
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }
-    }
-    catch (error) {
-      console.log(error)
-    }
-  }
-
 
   // Render Methods
   function Wallet() {
@@ -204,44 +142,6 @@ function App() {
     );
   }
 
-  function TimeBox(props) {
-    const [timeleft, setTimeleft] = useState(0);
-
-    useEffect(() => {
-      let timer = setInterval(() => {
-        setTimeleft((new Date(props.closingTime)) - (new Date()));
-      }, 1000);
-
-      return function cleanup() {
-        clearInterval(timer);
-      }
-    });
-
-    return (
-      <div className='row sale-row timeBox-row'>
-        <div className='col-3 timeBox mx-auto'>
-          <p className='timeHeader'>{Math.floor(timeleft / (1000 * 60 * 60 * 24))}</p>
-          <p className='timeBody'>DAYS</p>
-        </div>
-
-        <div className='col-3 timeBox mx-auto'>
-          <p className='timeHeader'>{Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}</p>
-          <p className='timeBody'>HOURS</p>
-        </div>
-
-        <div className='col-3 timeBox mx-auto'>
-          <p className='timeHeader'>{Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60))}</p>
-          <p className='timeBody'>MINS</p>
-        </div>
-
-        <div className='col-3 timeBox mx-auto'>
-          <p className='timeHeader'>{Math.floor((timeleft % (1000 * 60)) / 1000)}</p>
-          <p className='timeBody'>SECS</p>
-        </div>
-      </div>
-    );
-  }
-
   function Buy(props) {
     const [tokenAmount, setTokenAmount] = useState("");
 
@@ -253,93 +153,81 @@ function App() {
       if (tokenAmount >= props.min && tokenAmount <= props.max) {
         buyToken(tokenAmount);
         setTokenAmount("");
+        getBalance
       }
+      
     }
 
     return (
-
       <div>
         <div className='row sale-row'>
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text">ETH</span>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text">TEST Token</span>
             </div>
 
-            <input type="number" min={props.min} max={props.max} step="any" onChange={handleAmountInput} value={tokenAmount} class="form-control" placeholder="Enter Amount" />
+            <input type="number" min={props.min} max={props.max} step="any" onChange={handleAmountInput} value={tokenAmount} className="form-control" placeholder="Enter Amount" />
 
           </div>
         </div>
 
         <div className='row sale-row'>
           <button onClick={handleBuy} className="cta-button connect-wallet-button">
-            Buy JoviToken
+            Buy TEST_TOKEN
           </button>
         </div>
       </div>
     );
   }
 
+  function Withdraw() {
+    const [withdrawAmount, setWithdrawAmount] = useState("");
 
-  function TokenSupply(props) {
-    const [totalSupply, setTotalSupply] = useState("");
-    const [stateChange, setStateChange] = useState(false);
+    function handleAmountInput(e) {
+      setWithdrawAmount(e.target.value);
+    }
 
-    useEffect(() => {
-      (async () => {
-        await registerTotalSupplyChange(() => {
-          setStateChange(!stateChange);
-        });
-        setTotalSupply(await getTotalSupply());
-      })();
-    });
+    function handleWithdraw() {
+      withdrawToken(withdrawAmount);
+      setWithdrawAmount("");
+    }
 
     return (
-      <p className='saleDesc'>{props.stage} Supply: <span class="saleDescVal">{totalSupply}</span></p>
+      <div>
+        <div className='row sale-row'>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text">TEST Token</span>
+            </div>
+            <input type="number" step="any" onChange={handleAmountInput} value={withdrawAmount} className="form-control" placeholder="Enter Amount" />
+
+          </div>
+        </div>
+
+        <div className='row sale-row'>
+          <button onClick={handleWithdraw} className="cta-button connect-wallet-button">
+            Withdraw TEST_TOKEN
+          </button>
+        </div>
+      </div>
     );
   }
 
   function SaleCard() {
-    const [closingTime, setClosingTime] = useState("");
-    const [stage, setStage] = useState("");
-    const [rate, setRate] = useState();
-    const [investorMinAmount, setInvestorMinAmount] = useState("");
-    const [investorMaxAmount, setInvestorMaxAmount] = useState("");
-
-    useEffect(() => {
-
-      (async () => {
-        const crowdsaleData = await getCrowdsaleData();
-        setClosingTime(crowdsaleData.closingTime);
-        setStage(crowdsaleData.stage);
-        setRate(crowdsaleData.rate);
-        setInvestorMinAmount(crowdsaleData.investorMinAmount);
-        setInvestorMaxAmount(crowdsaleData.investorMaxAmount);
-
-      })();
-
-    });
-
     return (
-
       (currentAccount === "") ? (
         ""
       ) : (
         <div className="col-lg-6">
           <div className="saleCard">
             <div className='row sale-row'>
-              <p className="SaleHeader">{stage} Ends in: </p>
+              <p className='saleDesc'>Account Balance: <span className="saleDescVal">{balanceAmout.Account_balance}</span></p>
+              <p className='saleDesc'>Token Name: <span className="saleDescVal">TEST TOKEN</span></p>
+              <p className='saleDesc'>Token Balanace: <span className="saleDescVal">{balanceAmout.ICO_balance}</span></p>
+              <p className='saleDesc'>Price: <span className="saleDescVal">1 TEST = 0.015 METC</span></p>
             </div>
-
-            <TimeBox closingTime={closingTime} />
-
-            <div className='row sale-row'>
-              <p className='saleDesc'>Token Name: <span class="saleDescVal">JOVI</span></p>
-              <TokenSupply stage={stage} />
-              <p className='saleDesc'>{stage} Price: <span class="saleDescVal">1 ETH = {rate} JOVI</span></p>
-            </div>
-
-            <Buy min={investorMinAmount} max={investorMaxAmount} />
-
+            <Buy min={100} max={500} />
+            <Withdraw />
           </div>
         </div>
       )
@@ -354,32 +242,26 @@ function App() {
     <div className="App container-fluid">
       <nav className="navbar">
         <div className="header-container">
-          <p className="header gradient-text">JoviToken</p>
+          <p className="header gradient-text">TestToken</p>
         </div>
-
         <div className="wallet-container">
           {Wallet()}
         </div>
-
-
       </nav>
 
       <div className="row app-row app-container">
         <div className="col-lg-6">
           <p className="sub-text">
-            Buy JoviToken Now, To Get Rich In The Future.
+            Buy TestToken Now, To Get Rich In The Future.
           </p>
 
           <p className="sub-sub-text">
-            JoviToken is more than just a Defi Token. It's the best DeFi Token and you can learn here about this crypto
+          TestToken is more than just a Defi Token. It's the best DeFi Token and you can learn here about this crypto
           </p>
 
         </div>
 
-
-
         {SaleCard()}
-
 
         {Footer()}
 
